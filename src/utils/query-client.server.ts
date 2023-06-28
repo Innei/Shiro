38f1bf6f6876bd909b9c9da17390667cdcbd473f
Innei <i@innei.in>
2023-06-28 11:46:28 +0800

    fix: request scope queryclient
    Signed-off-by: Innei <i@innei.in>

--- src/utils/query-client.server.ts
+++ src/utils/query-client.server.ts
@@ -1,12 +1,11 @@
 import { QueryClient } from '@tanstack/react-query'
-import { cache } from 'react'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 3,
-      cacheTime: 1000 * 3,
+export const getQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 3,
+        cacheTime: 1000 * 3,
+      },
     },
-  },
-})
-export const getQueryClient = cache(() => queryClient)
+  })
