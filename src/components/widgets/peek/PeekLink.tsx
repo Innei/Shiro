703cde8a5d0c05c8fa144b8bdd718ddc7eb634fb
Innei <i@innei.in>
2023-07-04 12:40:33 +0800

    fix: peek link
    Signed-off-by: Innei <i@innei.in>

--- src/components/widgets/peek/PeekLink.tsx
+++ src/components/widgets/peek/PeekLink.tsx
@@ -6,9 +6,7 @@ import type { FC, PropsWithChildren, SyntheticEvent } from 'react'
 import { useIsMobile } from '~/atoms'
 import { useModalStack } from '~/providers/root/modal-stack-provider'
 
-import { NotePreview } from './NotePreview'
 import { PeekModal } from './PeekModal'
-import { PostPreview } from './PostPreview'
 
 export const PeekLink: FC<
   {
@@ -21,27 +19,31 @@ export const PeekLink: FC<
   const isMobile = useIsMobile()
   const { present } = useModalStack()
   const handlePeek = useCallback(
-    (e: SyntheticEvent) => {
+    async (e: SyntheticEvent) => {
       if (isMobile) return
 
       if (href.startsWith('/notes/')) {
-        {
-          e.preventDefault()
-          present({
-            clickOutsideToDismiss: true,
-            title: 'Preview',
-            modalClassName: 'flex justify-center',
-            modalContainerClassName: 'flex justify-center',
-            CustomModalComponent: () => (
-              <PeekModal to={href}>
-                <NotePreview noteId={parseInt(href.split('/').pop()!)} />
-              </PeekModal>
-            ),
-            content: () => null,
-          })
-        }
+        e.preventDefault()
+        const NotePreview = await import('./NotePreview').then(
+          (module) => module.NotePreview,
+        )
+        present({
+          clickOutsideToDismiss: true,
+          title: 'Preview',
+          modalClassName: 'flex justify-center',
+          modalContainerClassName: 'flex justify-center',
+          CustomModalComponent: () => (
+            <PeekModal to={href}>
+              <NotePreview noteId={parseInt(href.split('/').pop()!)} />
+            </PeekModal>
+          ),
+          content: () => null,
+        })
       } else if (href.startsWith('/posts/')) {
         e.preventDefault()
+        const PostPreview = await import('./PostPreview').then(
+          (module) => module.PostPreview,
+        )
         const splitpath = href.split('/')
         const slug = splitpath.pop()!
         const category = splitpath.pop()!
