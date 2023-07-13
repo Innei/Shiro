df8f3765535aaeeaf821d3e0da0351a8116de6e4
xiaomo <18272190626@163.com>
2023-07-13 19:34:20 +0800

    fix(style): <main> fix the floating layer (#38)

--- src/components/layout/content/Content.tsx
+++ src/components/layout/content/Content.tsx
@@ -1,6 +1,6 @@
 export const Content: Component = ({ children }) => {
   return (
-    <main className="relative z-[1] px-4 pt-[4.5rem] fill-content md:px-0">
+    <main className="relative z-[2] px-4 pt-[4.5rem] fill-content md:px-0">
       {children}
     </main>
   )
