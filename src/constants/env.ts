c294e4242253c06ea9e28a7917b80c4e2e58cf85
Innei <i@innei.in>
2023-07-15 15:16:25 +0800

    fix: type error

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
 import { isClientSide, isDev } from '~/lib/env'
 
-export const API_URL = (() => {
+export const API_URL: string = (() => {
   if (isDev) return process.env.NEXT_PUBLIC_API_URL
 
   if (isClientSide && process.env.NEXT_PUBLIC_CHINA_API_URL) {
@@ -8,5 +8,5 @@ export const API_URL = (() => {
   }
 
   return process.env.NEXT_PUBLIC_API_URL || '/api/v2'
-})()
+})() as string
 export const GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL || ''
