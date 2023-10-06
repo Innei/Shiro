56c9d716daf3df516075f44221ea1fb288cce02b
TNXG <tianxiang_tnxg@outlook.com>
2023-10-07 04:26:25 +0800

    Feat: 修改文案

--- src/components/layout/footer/FooterInfo.tsx
+++ src/components/layout/footer/FooterInfo.tsx
@@ -15,9 +15,9 @@ import { VercelPoweredBy } from './VercelPoweredBy'
 const isVercelEnv = !!process.env.NEXT_PUBLIC_VERCEL_ENV
 export const FooterInfo = () => {
   return (
-      <>
+    <>
       <div className="relative">
-          <FooterLinkSection />
+        <FooterLinkSection />
         {isVercelEnv && (
           <div className="absolute top-0 hidden lg:-right-8 lg:block">
             <VercelPoweredBy />
@@ -25,14 +25,14 @@ export const FooterInfo = () => {
         )}
       </div>
 
-        <FooterBottom />
+      <FooterBottom />
 
       {isVercelEnv && (
         <div className="mt-6 flex justify-center lg:hidden">
           <VercelPoweredBy />
         </div>
       )}
-      </>
+    </>
   )
 }
 
@@ -45,65 +45,65 @@ const FooterLinkSection = async () => {
   }
 
   return (
-      <div className="space-x-0 space-y-3 md:space-x-6 md:space-y-0">
-        {footerConfig.linkSections.map((section) => {
-          return (
-              <div className="block space-x-4 md:inline-flex" key={section.name}>
-                  <b className="font-medium">{section.name}</b>
-                  <span className="space-x-4 text-neutral-content\/90">
+    <div className="space-x-0 space-y-3 md:space-x-6 md:space-y-0">
+      {footerConfig.linkSections.map((section) => {
+        return (
+          <div className="block space-x-4 md:inline-flex" key={section.name}>
+            <b className="font-medium">{section.name}</b>
+            <span className="space-x-4 text-neutral-content\/90">
               {section.links.map((link) => {
                 return (
-                    <StyledLink
-                        external={link.external}
-                        className="link-hover link"
-                        href={link.href}
-                        key={link.name}
-                    >
-                      {link.name}
-                    </StyledLink>
+                  <StyledLink
+                    external={link.external}
+                    className="link-hover link"
+                    href={link.href}
+                    key={link.name}
+                  >
+                    {link.name}
+                  </StyledLink>
                 )
               })}
             </span>
-              </div>
-          )
-        })}
-      </div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
 const StyledLink = (
-    props: JSX.IntrinsicElements['a'] & {
-      external?: boolean
-    },
+  props: JSX.IntrinsicElements['a'] & {
+    external?: boolean
+  },
 ) => {
   const { external, ...rest } = props
   const As = external ? 'a' : Link
 
   return (
-      // @ts-ignore
-      <As
-          className="link-hover link"
-          target={props.external ? '_blank' : props.target}
-          {...rest}
-      >
-        {props.children}
-      </As>
+    // @ts-ignore
+    <As
+      className="link-hover link"
+      target={props.external ? '_blank' : props.target}
+      {...rest}
+    >
+      {props.children}
+    </As>
   )
 }
 const Divider: Component = ({ className }) => {
   return (
-      <span className={clsxm('select-none whitespace-pre opacity-50', className)}>
+    <span className={clsxm('select-none whitespace-pre opacity-50', className)}>
       {' '}
-        |{' '}
+      |{' '}
     </span>
   )
 }
 
 const PoweredBy: Component = ({ className }) => {
   return (
-      <span className={className}>
+    <span className={className}>
       Powered by{' '}
-        <StyledLink href="https://github.com/mx-space" target="_blank">
+      <StyledLink href="https://github.com/mx-space" target="_blank">
         Mix Space
       </StyledLink>
       . <Divider />
@@ -147,14 +147,14 @@ const FooterBottom = async () => {
   const { date = currentYear, icp } = otherInfo || {}
 
   return (
-      <div className="mt-12 space-y-3 text-center md:mt-6 md:text-left">
-        <p>
-          <span>© {date.replace('{{now}}', currentYear)} </span>
-          <a href="/">
-            <OwnerName />
-          </a>
-          <span>.</span>
-          <span>
+    <div className="mt-12 space-y-3 text-center md:mt-6 md:text-left">
+      <p>
+        <span>© {date.replace('{{now}}', currentYear)} </span>
+        <a href="/">
+          <OwnerName />
+        </a>
+        <span>.</span>
+        <span>
           <Divider />
           <a href="/feed" target="_blank">
             RSS
@@ -163,30 +163,30 @@ const FooterBottom = async () => {
           <a href="/sitemap.xml" target="_blank">
             站点地图
           </a>
-          <Divider className="hidden md:inline" />
-
-          <SubscribeTextButton>
-            <Divider className="inline" />
-          </SubscribeTextButton>
+          <a>
+            <SubscribeTextButton>
+              <Divider className="inline" />
+            </SubscribeTextButton>
+          </a>
         </span>
-          <span className="mt-3 block md:mt-0 md:inline">
+        <span className="mt-3 block md:mt-0 md:inline">
           Live in the present more than the future or the past.
         </span>
-        </p>
-        <p>
-          <PoweredBy className="my-3 block md:my-0 md:inline" />
-          {icp && (
-              <>
-                <Divider className="hidden md:inline" />
-                <StyledLink href={icp.link} target="_blank" rel="noreferrer">
-                  {icp.text}
-                </StyledLink>
-              </>
-          )}
+      </p>
+      <p>
+        <PoweredBy className="my-3 block md:my-0 md:inline" />
+        {icp && (
+          <>
+            <Divider className="hidden md:inline" />
+            <StyledLink href={icp.link} target="_blank" rel="noreferrer">
+              {icp.text}
+            </StyledLink>
+          </>
+        )}
 
-          <Divider className="hidden md:inline" />
-          <GatewayCount />
-          {/* {!!lastVisitor && (
+        <Divider className="hidden md:inline" />
+        <GatewayCount />
+        {/* {!!lastVisitor && (
           <>
             <Divider />
             <span>
@@ -198,7 +198,7 @@ const FooterBottom = async () => {
             </span>
           </>
         )} */}
-        </p>
-      </div>
+      </p>
+    </div>
   )
 }
