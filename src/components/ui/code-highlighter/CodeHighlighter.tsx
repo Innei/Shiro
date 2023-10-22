1b0b3fa93a4b46cecb4ec717696f37779d6c87e8
TNXG <tianxiang_tnxg@outlook.com>
2023-10-22 20:58:16 +0800

    🐞 fix: 尝试修复代码高亮

--- src/components/ui/code-highlighter/CodeHighlighter.tsx
+++ src/components/ui/code-highlighter/CodeHighlighter.tsx
@@ -1,66 +1,48 @@
-// 导入React和一些React钩子和函数
-
-// 导入一个自定义钩子，用于检查当前模式是否为打印模式
-// 导入一个自定义钩子，用于检查当前主题是否为深色
-// 导入自定义函数，用于加载脚本和样式表
-// 导入一个自定义弹出通知工具
-
-// 导入此组件的CSS模块样式
-
+import React, { useEffect, useState } from 'react'
 import {
   createShikiHighlighter,
   renderCodeToHTML,
   runTwoSlash,
 } from 'shiki-twoslash'
-import type { FC } from 'react' // 从React中导入FC类型，用于函数式组件
+import type { FC } from 'react'
 
-// 声明一个全局接口，用于在'window'对象上定义'Prism'属性
 declare global {
   interface Window {
     Prism: any
   }
 }
 
-// 定义组件的属性接口
 interface Props {
-  lang: string // 代码片段的语言（或未定义）
-  content: string // 要显示的代码内容
+  lang: string | undefined
+  content: string
 }
 
-// 定义接受'Props'的函数式组件'HighLighter'
 export const HighLighter: FC<Props> = (props) => {
-  const { lang: language, content: value } = props // 从'props'中解构'lang'和'content'
+  const { lang: language, content: value } = props
+  const [html, setHtml] = useState<string | null>(null)
 
-  // // 定义一个用于将代码复制到剪贴板的回调函数
-  // const handleCopy = useCallback(() => {
-  //   navigator.clipboard.writeText(value); // 使用Clipboard API将代码复制到剪贴板
-  //   toast.success('已复制！'); // 使用'toast'工具显示成功消息
-  // }, [value]);
+  useEffect(() => {
+    const highlightCode = async () => {
+      const highlighter = await createShikiHighlighter({ theme: 'dark-plus' })
+      const twoslash = runTwoSlash(value, language, {})
+      const renderedHtml = renderCodeToHTML(
+        twoslash.code,
+        language || 'markup',
+        { twoslash: true },
+        { themeName: 'dark-plus' },
+        highlighter,
+        twoslash,
+      )
+      setHtml(renderedHtml)
+    }
 
-  // // 检查当前模式是否为打印模式
-  // const isPrintMode = useIsPrintMode();
+    highlightCode()
+  }, [language, value])
 
-  // // 检查当前主题是否为深色
-  // const isDark = useIsDark();
-  // useInsertionEffect(() => {
-  //   const css = loadStyleSheet(
-  //     `https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/prism-themes/1.9.0/prism-one-${
-  //       isPrintMode ? 'light' : isDark ? 'dark' : 'light'
-  //     }.css`,
-  //   );
-  const go = async () => {
-    const highlighter = await createShikiHighlighter({ theme: 'dark-plus' })
-    const twoslash = runTwoSlash(value, language, {})
-    const html = renderCodeToHTML(
-      twoslash.code,
-      language,
-      { twoslash: true },
-      { themeName: 'dark-plus' },
-      highlighter,
-      twoslash,
-    )
-    return html
+  if (html === null) {
+    // 返回一个占位符或加载状态
+    return <div>Loading...</div>
   }
 
-  return go()
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
