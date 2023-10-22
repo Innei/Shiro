689431ec4f603e783026b13ebcd31037977750b1
TNXG <tianxiang_tnxg@outlook.com>
2023-10-22 22:25:43 +0800

    🐞 fix: 尝试修复代码高亮

--- src/components/ui/code-highlighter/CodeHighlighter.tsx
+++ src/components/ui/code-highlighter/CodeHighlighter.tsx
@@ -1,53 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import {
-  createShikiHighlighter,
-  renderCodeToHTML,
-  runTwoSlash,
-  setCDN, // 从shiki-twoslash导入setCDN
-} from 'shiki-twoslash'
-import type { FC } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
+import { shiki } from 'shiki'
 
-declare global {
-  interface Window {
-    Prism: any
-  }
-}
+import { toast } from '~/lib/toast' // 请根据项目结构调整导入路径
 
 interface Props {
   lang: string | undefined
   content: string
 }
 
-export const HighLighter: FC<Props> = (props) => {
+shiki.setCDN('https://npm.onmicrosoft.cn/shiki/') // 设置 Shiki CDN 地址
+
+const CodeHighlighter = (props: Props) => {
   const { lang: language, content: value } = props
-  const [html, setHtml] = useState<string | null>(null)
+
+  // 处理复制到剪贴板的函数
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(value)
+    toast.success('COPIED！已复制！')
+  }, [value])
+
+  // 创建一个 ref 用于将高亮后的代码插入到 DOM 中
+  const ref = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
-    const highlightCode = async () => {
-      // 指定CDN提供程序，例如GitHub CDN
-      setCDN('https://npm.onmicrosoft.cn/shiki/')
+    const theme = 'dark_plus'
 
-      const highlighter = await createShikiHighlighter({ theme: 'dark-plus' })
+    // 在 Shiki 加载完成后执行高亮代码的操作
+    shiki.onLoad(async () => {
+      const shikiTheme = await shiki.getTheme(theme) // 获取 Shiki 主题
 
-      const twoslash = runTwoSlash(value, language || 'markup', {})
-      const renderedHtml = renderCodeToHTML(
-        twoslash.code,
-        language || 'markup',
-        { twoslash: true },
-        { themeName: 'dark-plus' },
-        highlighter,
-        twoslash,
+      // 设置代码高亮主题
+      shiki.setTheme(shikiTheme)
+
+      // 创建 Shiki 高亮器对象
+      const highlighter = await shiki.getHighlighter({
+        theme: shikiTheme,
+        langs: language, // 选择需要支持的编程语言
+      })
+
+      // 使用 Shiki 高亮器将代码转换为 HTML
+      const highlightedCode = highlighter.codeToHtml(value, language)
+
+      return (
+        <pre>
+          <code
+            className={`language-${language ?? 'markup'}`}
+            dangerouslySetInnerHTML={{ __html: highlightedCode }}
+          />
+        </pre>
       )
-      setHtml(renderedHtml)
-    }
-
-    highlightCode()
+    })
   }, [language, value])
-
-  if (html === null) {
-    // 返回一个占位符或加载状态
-    return <div>Loading...</div>
-  }
-
-  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
+
+export default CodeHighlighter
