b7c3a5a39cdab37997569209076144b53f25bcd3
TNXG <tianxiang_tnxg@outlook.com>
2023-10-22 23:04:31 +0800

    🐞 fix: 尝试修复代码高亮

--- src/components/ui/code-highlighter/render.server.ts
+++ src/components/ui/code-highlighter/render.server.ts
@@ -5,7 +5,7 @@ export const renderCodeHighlighter = async (
   lang: string,
   theme: string, // theme 参数是一个字符串
 ) => {
-  setCDN('https://npm.onmicrosoft.cn/shiki/') // 设置 Shiki CDN 地址
+  setCDN('https://jsd.onmicrosoft.cn/npm/shiki@0.14.5/') // 设置 Shiki CDN 地址
   // 使用映射函数将字符串主题转换为 Shiki 主题
   return await getHighlighter({
     langs: [lang as any],
