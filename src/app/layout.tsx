1c73bf29dc8153314944bfd6a6e907b476147385
TNXG <tianxiang_tnxg@outlook.com>
2023-10-27 20:08:09 +0800

    🐞 fix: 捉虫子

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,45 +139,37 @@ export default async function RootLayout(props: Props) {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-      // 如果有旧版本的SW，先卸载
-      if (localStorage.getItem('sw_installed') === 'true') {
-        console.log('[TNXG_SW]检测到旧版本的SW，正在卸载...');
-        navigator.serviceWorker.getRegistrations()
-          .then(function(registrations) {
-            for (let registration of registrations) {
-              registration.unregister();
+              if (localStorage.getItem('sw_installed') === 'true') {
+                console.log('[TNXG_SW]检测到旧版本的SW，正在卸载...');
+                navigator.serviceWorker.getRegistrations()
+                    .then(function (registrations) {
+                        for (let registration of registrations) {
+                            registration.unregister();
+                        }
+                    });
+                navigator.serviceWorker.register('/sw.js?t=' + new Date().getTime())
+            } else {
+                if (!!navigator.serviceWorker) {
+                    navigator.serviceWorker.register('/sw.js?t=' + new Date().getTime())
+                        .then(async (registration) => {
+                            if (localStorage.getItem('sw_installed') !== 'true') {
+                                localStorage.setItem('sw_installed', 'true');
+                                console.log('[TNXG_SW] 安装成功，正在重载页面！');
+                                fetch(window.location.href)
+                                    .then(res => res.text())
+                                    .then(text => {
+                                        document.open();
+                                        document.write(text);
+                                        document.close();
+                                    });
+                            }
+                        }).catch(err => {
+                            console.error('[TNXG_SW] 安装失败，原因： ' + err.message);
+                        });
+                } else {
+                    console.error('[TNXG_SW] 安装失败，原因： 浏览器不支持service worker');
+                }
             }
-          });
-        localStorage.removeItem('sw_installed');
-        fetch(window.location.href)
-          .then(res => res.text())
-          .then(text => {
-            document.open();
-            document.write(text);
-            document.close();
-          });
-      } else {
-        if (!!navigator.serviceWorker) {
-          navigator.serviceWorker.register('/sw.js?t=' + new Date().getTime())
-            .then(async (registration) => {
-              if (localStorage.getItem('sw_installed') !== 'true') {
-                localStorage.setItem('sw_installed', 'true');
-                console.log('[TNXG_SW] 安装成功，正在重载页面！');
-                fetch(window.location.href)
-                  .then(res => res.text())
-                  .then(text => {
-                    document.open();
-                    document.write(text);
-                    document.close();
-                  });
-              }
-            }).catch(err => {
-              console.error('[TNXG_SW] 安装失败，原因： ' + err.message);
-            });
-        } else {
-          console.error('[TNXG_SW] 安装失败，原因： 浏览器不支持service worker');
-        }
-      }
     `,
             }}
           />
