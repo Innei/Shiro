0bf51cd8cf8457a2666906dea4a333081fc2a2b8
TNXG <tianxiang_tnxg@outlook.com>
2023-10-27 16:02:17 +0800

    feat: add activity app icons

--- src/components/layout/header/internal/Activity.tsx
+++ src/components/layout/header/internal/Activity.tsx
@@ -44,6 +44,13 @@ const appDescription = {
   'Microsoft Excel': '雕花',
   'Microsoft PowerPoint': '画饼',
   SimplyPiano: '练琴',
+
+  "Microsoft Edge": 'Microsoft 边缘(',
+  umamusume: '闪耀！优俊少女，启动！',
+  WindowsTerminal: 'del /f /s /q c:/ (不是',
+  "卡拉彼丘": '卡拉彼丘！启动！',
+  "Yuanshen.exe": '原神！启动！',
+
 } as any
 const appLabels: { [app: string]: string } = {
   'Activity Monitor': 'activity',
@@ -122,6 +129,14 @@ const appLabels: { [app: string]: string } = {
   Thorium: 'thorium',
 
   cmusic: 'cmusic',
+
+  "Microsoft Edge": "edge",
+  firefox: 'firefox',
+  idea64: 'idea',
+  "Explorer.EXE": 'windows_explorer',
+  WindowsTerminal: 'windows_terminal',
+  "卡拉彼丘":"calatopia",
+  "Yuanshen.exe":"genshin",
 }
 // autocorrect: true
 export const Activity = memo(() => {
