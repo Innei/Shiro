4792d89259432738749c11c356e606637621bf35
TNXG <tianxiang_tnxg@outlook.com>
2023-10-28 15:28:55 +0800

    2333

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,190 +1,233 @@
 // 添加监听器 / AddEventListener
-const CACHE_NAME = 'TNXG_BLOG_CACHE';
-let cachelist = [];
+const ServiceWoker_Version = '23.10.28-rc.1'
+const CACHE_NAME = 'TNXG_BLOG_CACHE'
+let cachelist = []
 self.addEventListener('install', async function (installEvent) {
-    self.skipWaiting();
-    installEvent.waitUntil(caches.open(CACHE_NAME)
-        .then(function (cache) {
-            console.log('[TNXG_SW]Opened cache');
-            return cache.addAll(cachelist);
-        }));
-});
-self.addEventListener('fetch', async event => {
-    event.respondWith(handle(event.request))
-});
+  self.skipWaiting()
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('[TNXG_SW]Opened cache')
+      return cache.addAll(cachelist)
+    }),
+  )
+})
+self.addEventListener('fetch', async (event) => {
+  event.respondWith(handle(event.request))
+})
 const handle = async (req) => {
-    const urlStr = req.url
-    const urlObj = new URL(urlStr);
-    const urlPath = urlObj.pathname;
-    const domain = urlObj.hostname;
+  const urlStr = req.url
+  const urlObj = new URL(urlStr)
+  const urlPath = urlObj.pathname
+  const domain = urlObj.hostname
 
-    self.db = {
-        read: (key, config) => {
-            if (!config) {
-                config = { type: "text" }
-            }
-            return new Promise((resolve, reject) => {
-                caches.open(CACHE_NAME).then(cache => {
-                    cache.match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`)).then(function (res) {
-                        if (!res) resolve(null)
-                        res.text().then(text => resolve(text))
-                    }).catch(() => {
-                        resolve(null)
-                    })
-                })
+  self.db = {
+    read: (key, config) => {
+      if (!config) {
+        config = { type: 'text' }
+      }
+      return new Promise((resolve, reject) => {
+        caches.open(CACHE_NAME).then((cache) => {
+          cache
+            .match(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`))
+            .then(function (res) {
+              if (!res) resolve(null)
+              res.text().then((text) => resolve(text))
             })
-        }, write: (key, value) => {
-            return new Promise((resolve, reject) => {
-                caches.open(CACHE_NAME).then(function (cache) {
-                    cache.put(new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`), new Response(value));
-                    resolve()
-                }).catch(() => {
-                    reject()
-                })
+            .catch(() => {
+              resolve(null)
             })
-        }
+        })
+      })
+    },
+    write: (key, value) => {
+      return new Promise((resolve, reject) => {
+        caches
+          .open(CACHE_NAME)
+          .then(function (cache) {
+            cache.put(
+              new Request(`https://LOCALCACHE/${encodeURIComponent(key)}`),
+              new Response(value),
+            )
+            resolve()
+          })
+          .catch(() => {
+            reject()
+          })
+      })
+    },
+  }
+  // 对数组内所有地址进行请求，返回第一个成功的请求结果并打断其他请求
+  const 并发请求 = async (urls, url) => {
+    let controller = new AbortController()
+    const PauseProgress = async (res) => {
+      return new Response(await res.arrayBuffer(), {
+        status: res.status,
+        headers: res.headers,
+      })
     }
-    // 对数组内所有地址进行请求，返回第一个成功的请求结果并打断其他请求
-    const 并发请求 = async (urls, url) => {
-        let controller = new AbortController();
-        const PauseProgress = async (res) => {
-            return new Response(await (res).arrayBuffer(), { status: res.status, headers: res.headers });
-        };
-        if (!Promise.any) {
-            Promise.any = function (promises) {
-                return new Promise((resolve, reject) => {
-                    promises = Array.isArray(promises) ? promises : []
-                    let len = promises.length
-                    let errs = []
-                    if (len === 0) return reject(new AggregateError('All promises were rejected'))
-                    promises.forEach((promise) => {
-                        promise.then(value => {
-                            resolve(value)
-                        }, err => {
-                            len--
-                            errs.push(err)
-                            if (len === 0) {
-                                reject(new AggregateError(errs))
-                            }
-                        })
-                    })
-                })
-            }
-        }
-        return Promise.any(urls.map(urls => {
-            return new Promise((resolve, reject) => {
-                fetch(urls, {
-                    signal: controller.signal
-                })
-                    .then(PauseProgress)
-                    .then(res => {
-                        if (res.status == 200 || res.status == 304 || res.status == 404) {
-                            controller.abort()
-                            resolve(res)
-                        } else {
-                            reject(res)
-                        }
-                    })
-                    .catch(e => reject(e))
-            })
-        }))
-    }
-    // 检查是否是 tnxg.top 域名，否则返回警告
-    self.addEventListener('message', event => {
-        const currentURL = event.data;
-        console.log('[TNXG_SW]检测到消息：' + currentURL);
-        if (currentURL !== 'tnxg.top') {
-            return new Response('Access denied. Only \'tnxg.top\' is allowed.', {
-                status: 403,
-                headers: { 'Content-Type': 'text/plain' }
-            });
-        }
-    });
-
-    // 主站api函数
-    // 拦截所有路径为域名/sw-req/的请求
-    if (req.url.includes('/sw-req/')) {
-        console.log('[TNXG_SW]检测到SW请求：' + req.url);
-        let res = new Response('{"code":200,"msg":"TNXG_SW"}', {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        // 拦截所有路径为域名/api?getip的请求
-        if (req.url.includes('/api?getip')) {
-            return fetch('https://api.ip.sb/geoip')
-        }
-        return res;
-    }
-
-    // 网络请求处理函数
-    if (req.url.includes('hdslb.com')) {
-        if (req.url.includes("HarmonyOS")) {
-            return fetch(req)
-        }
-        // 获取路径;
-        const path = req.url.replace(/(https|http)?:\/\/(.[^/]+)/, '');
-        const 站点镜像源 = [
-            `https://i0.hdslb.com`,
-            `https://i1.hdslb.com/`,
-            `https://i2.hdslb.com/`,
-            `https://s1.hdslb.com/`,
-            `https://s2.hdslb.com`,
-            `https://s3.hdslb.com/`,
-        ]
-        for (var i in 站点镜像源) {
-            站点镜像源[i] += path;
-        }
-        return 并发请求(站点镜像源);
-    }
-    // 天翔TNXG云存储处理函数
-    if (req.url.includes('https://assets.tnxg.whitenuo.cn')) {
-        console.log('[TNXG_SW]检测到网络请求：' + req.url);
-        // 天翔TNXG云存储图片WebP处理
-        if (req.headers.get('accept').includes('webp')) {
-            if (req.url.includes('.jpg') || req.url.includes('.png') || req.url.includes('.gif') || req.url.includes('.jpeg')) {
-                let webpReq = new Request(req.url + '?fmt=webp', req);
-                console.log('[TNXG_SW]检测到TNXG桶的图片请求[方式1]，转换WebP：' + req.url + '?fmt=webp');
-                return fetch(webpReq);
-            }
-        }
-    }
-    if (req.url.includes('/_next/image?url=https%3A%2F%2Fassets.tnxg.whitenuo.cn')) {
-        console.log('[TNXG_SW]检测到网络请求：' + req.url);
-        // 将常见图片存在数组中
-        const imgList = [
-            '.jpg',
-            '.png',
-            '.gif',
-            '.jpeg',
-        ]
-        // 如果url中包含常见图片后缀名则删除https://tnxg.top/_next/image?url=
-        for (var i in imgList) {
-            if (req.url.includes(imgList[i])) {
-                imgurl = req.url.replace('https://tnxg.top/_next/image?url=', '');
-                imgurl = decodeURIComponent(imgurl);
-                console.log('[TNXG_SW]检测到Shiro图片代理链接，转换为直链：' + imgurl);
-                imgurl = imgurl.split('&')[0];
-                if (req.headers.get('accept').includes('webp')) {
-                    let webpReq = new Request(imgurl + '?fmt=webp', req);
-                    console.log('[TNXG_SW]检测到TNXG桶的图片请求[方式2]，转换WebP：' + imgurl + '?fmt=webp');
-                    return fetch(webpReq);
-                } else {
-                    return fetch(req);
+    if (!Promise.any) {
+      Promise.any = function (promises) {
+        return new Promise((resolve, reject) => {
+          promises = Array.isArray(promises) ? promises : []
+          let len = promises.length
+          let errs = []
+          if (len === 0)
+            return reject(new AggregateError('All promises were rejected'))
+          promises.forEach((promise) => {
+            promise.then(
+              (value) => {
+                resolve(value)
+              },
+              (err) => {
+                len--
+                errs.push(err)
+                if (len === 0) {
+                  reject(new AggregateError(errs))
                 }
-            }
-        }
+              },
+            )
+          })
+        })
+      }
+    }
+    return Promise.any(
+      urls.map((urls) => {
+        return new Promise((resolve, reject) => {
+          fetch(urls, {
+            signal: controller.signal,
+          })
+            .then(PauseProgress)
+            .then((res) => {
+              if (res.status == 200 || res.status == 304 || res.status == 404) {
+                controller.abort()
+                resolve(res)
+              } else {
+                reject(res)
+              }
+            })
+            .catch((e) => reject(e))
+        })
+      }),
+    )
+  }
+  // 检查是否是 tnxg.top 域名，否则返回警告
+  self.addEventListener('message', (event) => {
+    const currentURL = event.data
+    console.log('[TNXG_SW]检测到消息：' + currentURL)
+    if (currentURL !== 'tnxg.top') {
+      return new Response("Access denied. Only 'tnxg.top' is allowed.", {
+        status: 403,
+        headers: { 'Content-Type': 'text/plain' },
+      })
+    }
+  })
+
+  // 主站api函数
+  // 拦截所有路径为域名/sw-req/的请求
+  if (req.url.includes('/sw-req/')) {
+    console.log('[TNXG_SW]检测到SW请求：' + req.url)
+    let res = new Response('{"code":200,"msg":"TNXG_SW"}', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    // 拦截所有路径为域名/api?getip的请求
+    if (req.url.includes('/api?getip')) {
+      return fetch('https://api.ip.sb/geoip')
     }
 
-    if (req.url.includes('jsdelivr.net')) {
-        console.log('[TNXG_SW]检测到网络请求：' + req.url);
-        // 获取请求路径
-        const path = req.url.replace(/(https|http)?:\/\/(.[^/]+)/, '');
-        const jsdelivr_mirror = [
-            `https://cdn2.chuqis.com` + path,
-            `https://jsd.onmicrosoft.cn` + path,
-        ]
-        return 并发请求(jsdelivr_mirror);
+    if (req.url.includes('/api?version')) {
+      let res = new Response(
+        `{"code":200,"msg":"TNXG_SW,"version":"${ServiceWoker_Version}"}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      return res
     }
-    return fetch(req)
+    return res
+  }
+
+  // 网络请求处理函数
+  if (req.url.includes('hdslb.com')) {
+    if (req.url.includes('HarmonyOS')) {
+      return fetch(req)
+    }
+    // 获取路径;
+    const path = req.url.replace(/(https|http)?:\/\/(.[^/]+)/, '')
+    const 站点镜像源 = [
+      `https://i0.hdslb.com`,
+      `https://i1.hdslb.com/`,
+      `https://i2.hdslb.com/`,
+      `https://s1.hdslb.com/`,
+      `https://s2.hdslb.com`,
+      `https://s3.hdslb.com/`,
+    ]
+    for (var i in 站点镜像源) {
+      站点镜像源[i] += path
+    }
+    return 并发请求(站点镜像源)
+  }
+  // 天翔TNXG云存储处理函数
+  if (req.url.includes('https://assets.tnxg.whitenuo.cn')) {
+    console.log('[TNXG_SW]检测到网络请求：' + req.url)
+    // 天翔TNXG云存储图片WebP处理
+    if (req.headers.get('accept').includes('webp')) {
+      if (
+        req.url.includes('.jpg') ||
+        req.url.includes('.png') ||
+        req.url.includes('.gif') ||
+        req.url.includes('.jpeg')
+      ) {
+        let webpReq = new Request(req.url + '?fmt=webp', req)
+        console.log(
+          '[TNXG_SW]检测到TNXG桶的图片请求[方式1]，转换WebP：' +
+            req.url +
+            '?fmt=webp',
+        )
+        return fetch(webpReq)
+      }
+    }
+  }
+  if (
+    req.url.includes('/_next/image?url=https%3A%2F%2Fassets.tnxg.whitenuo.cn')
+  ) {
+    console.log('[TNXG_SW]检测到网络请求：' + req.url)
+    // 将常见图片存在数组中
+    const imgList = ['.jpg', '.png', '.gif', '.jpeg']
+    // 如果url中包含常见图片后缀名则删除https://tnxg.top/_next/image?url=
+    for (var i in imgList) {
+      if (req.url.includes(imgList[i])) {
+        imgurl = req.url.replace('https://tnxg.top/_next/image?url=', '')
+        imgurl = decodeURIComponent(imgurl)
+        console.log('[TNXG_SW]检测到Shiro图片代理链接，转换为直链：' + imgurl)
+        imgurl = imgurl.split('&')[0]
+        if (req.headers.get('accept').includes('webp')) {
+          let webpReq = new Request(imgurl + '?fmt=webp', req)
+          console.log(
+            '[TNXG_SW]检测到TNXG桶的图片请求[方式2]，转换WebP：' +
+              imgurl +
+              '?fmt=webp',
+          )
+          return fetch(webpReq)
+        } else {
+          return fetch(req)
+        }
+      }
+    }
+  }
+
+  if (req.url.includes('jsdelivr.net')) {
+    console.log('[TNXG_SW]检测到网络请求：' + req.url)
+    // 获取请求路径
+    const path = req.url.replace(/(https|http)?:\/\/(.[^/]+)/, '')
+    const jsdelivr_mirror = [
+      `https://cdn2.chuqis.com` + path,
+      `https://jsd.onmicrosoft.cn` + path,
+    ]
+    return 并发请求(jsdelivr_mirror)
+  }
+  return fetch(req)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -141,26 +141,45 @@ export default async function RootLayout(props: Props) {
     // <ClerkProvider localization={ClerkZhCN}>
     <ClerkProvider>
       <html lang="zh-CN" className="noise" suppressHydrationWarning>
-        <ASCIIart />
         <head>
           <script
             dangerouslySetInnerHTML={{
               __html: `
               if (localStorage.getItem('sw_installed') === 'true') {
-                console.log('[TNXG_SW]检测到旧版本的SW，正在卸载...');
-                navigator.serviceWorker.getRegistrations()
-                    .then(function (registrations) {
-                        for (let registration of registrations) {
-                            registration.unregister();
+                fetch("/api?version")
+                    .then(res => res.json())
+                    .then(json => {
+                        if (json.version !== localStorage.getItem('sw_version')) {
+                            localStorage.setItem('sw_version', json.version);
+                            console.log('[TNXG_SW] 有新版本，正在重载页面！');
+                            fetch(window.location.href)
+                                .then(res => res.text())
+                                .then(text => {
+                                    document.open();
+                                    document.write(text);
+                                    document.close();
+                                });
+                        } else {
+                            console.log('[TNXG_SW] 无新版本，不重载页面！');
                         }
+                    }
+                    ).catch(err => {
+                        console.error('[TNXG_SW] 获取版本信息失败，原因： ' + err.message);
                     });
-                navigator.serviceWorker.register('/sw.js?t=' + new Date().getTime())
             } else {
                 if (!!navigator.serviceWorker) {
                     navigator.serviceWorker.register('/sw.js?t=' + new Date().getTime())
                         .then(async (registration) => {
                             if (localStorage.getItem('sw_installed') !== 'true') {
                                 localStorage.setItem('sw_installed', 'true');
+                                fetch("/api?version")
+                                    .then(res => res.json())
+                                    .then(json => {
+                                        localStorage.setItem('sw_version', json.version);
+                                    }
+                                    ).catch(err => {
+                                        console.error('[TNXG_SW] 获取版本信息失败，原因： ' + err.message);
+                                    });
                                 console.log('[TNXG_SW] 安装成功，正在重载页面！');
                                 fetch(window.location.href)
                                     .then(res => res.text())
@@ -210,67 +229,6 @@ export default async function RootLayout(props: Props) {
       <Analytics />
     </ClerkProvider>
   )
-}
-
-const ASCIIart = () => {
-  return `
-  &lt;!--
-    同人的诞生就是对于某个故事的意难平，正是因为这些意难平，众多优秀的作品才会从幕后走到台前。
-    这些故事的诞生就是为了让我们能够在这个世界上找到一些共鸣，找到一些温暖。
-    它们让我们看到了不同的可能性，让我们感受到了不同的情感。
-    它们，就是同人，就是本就不属于这个残酷世界的美好故事。
-    我们，热爱着这一切的人们，与现实有着不可跨越的鸿沟，这无疑使我们与他人更显得格格不入。
-    但是，我们却不会因此而放弃，我们却不会因此而停下脚步，我们会继续前行，去追寻那本不属于这个世界的，美好的故事。
---&gt;
-&lt;!--
-                          o                           *~+                      
-                     o ^n                       n * z                      
-                     . .**;                   -     o                      
-                    ^.  -  o      ..  ^^.   .v      ^                      
-                    .    i  nv             -1  +    z                      
-                     .   +     +~;    * z     v   *n                       
-                      in ^   .   -     .^~         -z   .                  
-                     a.    ^   .         *n   -       o   -                
-                    - ^   +              ~-z           z   *n              
-                  +^ -   -    ^             z          +*  n               
-                  ;       -  .+          - + z          -1o                
-                 v .    -     .          .. ^ -.. .      ;^                
-                 n - .   ;. . ^          .  ^   z         z*               
-               v ;-      + -^ n      -   *o o  ^  o    *^  a               
-           ;+   o.       ; no~ *+        v~-*a.   o+.+ ~-  ^n              
-                n..      ~^.uo  n       . ;v   n*         n~+aa            
-                ^~.     +** -1vnazn+     nv;azzvu         a~.              
-                ;v      v^~nz n   ; on*;     - zv ^       ao^              
-                n*       ;;  ^^ -~^  *   -      ~         o.^              
-                 ~   ..  .z-         ;         n +       ++                
-                 ~~  ;    oz                  n~a      ;.a-                
-                 z*o z^   ozv      *           u^   -~- o-^                
-                 ~* o^^-  ~  ++              u+-+    o++ .o                
-                  ;   zn ~;; -  .i-       u~  ~-   n+ ;. *o                
-                ~ ~    .  ..n + au+ ;1a+  zo.1o;   ..; . *~.               
-                + v     +  *   ^u    - z     ana   ;   - . ~               
-               * -z      * ^. vuz^     ~    o^a* -     ~;  *               
-               ^  v       -n~     .   ^*  v   u        un + ;              
-              ~ - .        ; a      o.;o~    z- .     - o^u*               
-              ..ao          ~n*      ^z^      ...     v *u o*o             
-             ;v .u^n n    .-+auz+1   *~  .n +;~^       *    ^ ~            
-            .-~    a- v+    .n +.  a;+;~~    .v    . ..+    -;.            
-            *n      .+v +;uuu^~ n~;z  a *~v  *    v +.       ; z           
-           ^ a        on   .~; -ono+v .~+ ^ ~**+ o*        - .* +          
-           * +         -.- ;-      aa*!     *;n; v            v v          
-          ~ *           -~  oiai.  ^^  n ^uiv*.^z ~           +.-.         
-          . ;            v          v            ;            +a n         
-         o; + .                      nv          a           o~  ~         
-             ;+     +                -;-         ~  -~       ;n  *         
-        n+   n*       .v  ;                         .         u  ^         
-        .~   ^v        ~;-*.                    a  ^          + *^.        
-       . v   **            nn                  1  v            u-++        
-       ~ z   ^               o  -;  ^  o -ozu.1 -              ~^^^        
-       ~+a   u               n      a~+o      ;                !*o-        
-       ~*o   i         1o-  -a        + **    no    *~no      u -v         
-       目白樱落 メジロさくらがちる Mejiro Sakuragachiru
---&gt;
-  `
 }
 
 const SayHi = () => {
