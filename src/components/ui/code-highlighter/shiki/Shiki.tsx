861fe474fdd61fd515c8afd4220e71d0f8a21ac3
Innei <i@innei.in>
2024-03-11 20:16:02 +0800

    fix: copy button style
    Signed-off-by: Innei <i@innei.in>

--- src/components/ui/code-highlighter/shiki/Shiki.tsx
+++ src/components/ui/code-highlighter/shiki/Shiki.tsx
@@ -140,7 +140,7 @@ export const ShikiHighLighter: FC<Props> = (props) => {
         <MotionButtonBase
           onClick={handleCopy}
           className={clsx(
-            'absolute right-2 top-2 z-[1] flex rounded border border-current p-2 text-xs center',
+            'absolute right-2 top-2 z-[1] flex rounded p-2 text-xs center',
             'rounded-md border border-accent/5 bg-accent/80 p-1.5 text-white backdrop-blur duration-200',
             'opacity-0 group-hover:opacity-100',
             filename && 'top-12',
