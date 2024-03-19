b4c74e3abaab1bf72eb56d64555f5e3d0fbd9ff8
Innei <i@innei.in>
2024-03-19 13:26:40 +0800

    fix: env
    Signed-off-by: Innei <i@innei.in>

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,8 +3,8 @@ import { isClientSide, isDev } from '~/lib/env'
 export const API_URL: string = (() => {
   if (isDev) return process.env.NEXT_PUBLIC_API_URL
 
-  if (isClientSide && process.env.NEXT_PUBLIC_CHINA_API_URL) {
-    return process.env.NEXT_PUBLIC_CHINA_API_URL
+  if (isClientSide && process.env.NEXT_PUBLIC_CLIENT_API_URL) {
+    return process.env.NEXT_PUBLIC_CLIENT_API_URL
   }
 
   return process.env.NEXT_PUBLIC_API_URL || '/api/v2'
