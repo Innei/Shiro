2e32ed00c787c7017000cb905fb89c0f633e176e
Innei <i@innei.in>
2024-03-29 00:09:40 +0800

    feat: build script
    Signed-off-by: Innei <i@innei.in>

---
+++ ci-release-build.sh
@@ -0,0 +1,22 @@
+#!env bash
+set -e
+CWD=$(pwd)
+
+npm run build
+cd .next
+pwd
+rm -rf cache
+cp -r ../public ./standalone/public
+
+cd ./standalone
+echo ';process.title = "Shiro (NextJS)"' >>server.js
+mv ../static/ ./.next/static
+
+cp $CWD/ecosystem.standalone.config.js ./ecosystem.config.js
+cp $CWD/.env.template .env
+
+cd ..
+
+mkdir -p $CWD/assets
+rm -rf $CWD/assets/release.zip
+zip --symlinks -r $CWD/assets/release.zip ./*
