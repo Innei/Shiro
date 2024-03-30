89b136a8332d33deb0335c87f9cc51157b130d3d
Innei <i@innei.in>
2024-03-30 23:02:08 +0800

    fix: cc
    Signed-off-by: Innei <i@innei.in>

--- src/components/modules/post/PostCopyright.tsx
+++ src/components/modules/post/PostCopyright.tsx
@@ -58,9 +58,9 @@ export const PostCopyright: FC = () => {
           本文采用
           <a
             className="shiro-link--underline"
-            href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh"
+            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
           >
-            知识共享署名 - 非商业性使用 - 相同方式共享 4.0 国际许可协议
+            CC BY-NC-SA 4.0 - 非商业性使用 - 相同方式共享 4.0 国际
           </a>
           进行许可。
         </p>
