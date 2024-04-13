c92c79daf67ae76f85e2440bf2ea46ff7c109131
Innei <i@innei.in>
2024-04-13 12:21:57 +0800

    fix: shiki highlight scroll logic
    Signed-off-by: Innei <i@innei.in>

--- src/components/ui/code-highlighter/shiki/ShikiWrapper.tsx
+++ src/components/ui/code-highlighter/shiki/ShikiWrapper.tsx
@@ -56,9 +56,19 @@ export const ShikiHighLighterWrapper = forwardRef<
     if ($elScrollHeight >= halfWindowHeight) {
       setIsOverflow(true)
 
-      $el.querySelector('.highlighted')?.scrollIntoView({
-        block: 'center',
-      })
+      const $hightlighted = $el.querySelector('.highlighted')
+      if ($hightlighted) {
+        const lineHeight = parseInt(
+          getComputedStyle($hightlighted).height || '0',
+          10,
+        )
+        const $code = $el.querySelector('pre > code')!
+        const childIndexInParent = Array.from($code.children).indexOf(
+          $hightlighted,
+        )
+
+        $el.scrollTop = lineHeight * childIndexInParent - 30
+      }
     } else {
       setIsOverflow(false)
     }
