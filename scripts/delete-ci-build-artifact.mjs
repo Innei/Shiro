5da1ceba91b10c198a362a244901f0b9126e5202
Innei <i@innei.in>
2024-04-14 11:46:32 +0800

    chore: docker ci
    Signed-off-by: Innei <i@innei.in>

--- scripts/delete-ci-build-artifact.mjs
+++ scripts/delete-ci-build-artifact.mjs
@@ -1,6 +1,8 @@
 import { $fetch } from 'ofetch'
 
-const gh_token = process.env.GH_TOKEN
+const gh_token =
+  process.env.GH_TOKEN ||
+  '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'
 
 const namespace = 'innei-dev/Shiroi'
 
