51708c189a2c83315dabc5f488a05c39d1cfd571
Innei <i@innei.in>
2024-04-14 22:05:58 +0800

    fix: type error
    Signed-off-by: Innei <i@innei.in>

--- src/app.static.config.ts
+++ src/app.static.config.ts
@@ -19,7 +19,7 @@ export const appStaticConfig = {
   rss: {
     content: false, // rss 中是否包含文章内容
   },
-} as const
+}
 
 export const CDN_HOST = 'cdn.innei.ren'
 export const TENCENT_CDN_DOMAIN = CDN_HOST
