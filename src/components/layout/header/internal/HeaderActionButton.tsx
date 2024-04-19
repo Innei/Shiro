73e503ffb6bff2a69dc65ddbfcc12b79243be682
Innei <i@innei.in>
2024-04-19 23:07:35 +0800

    fix: hydration error
    Signed-off-by: Innei <i@innei.in>

--- src/components/layout/header/internal/HeaderActionButton.tsx
+++ src/components/layout/header/internal/HeaderActionButton.tsx
@@ -2,11 +2,12 @@ import { forwardRef } from 'react'
 import clsx from 'clsx'
 
 export const HeaderActionButton = forwardRef<
-  HTMLButtonElement,
-  JSX.IntrinsicElements['button']
+  HTMLDivElement,
+  JSX.IntrinsicElements['div']
 >(({ children, ...rest }, ref) => {
   return (
-    <button
+    <div
+      role="button"
       className={clsx(
         'group size-10 rounded-full bg-gradient-to-b',
         'from-zinc-50/50 to-white/90 px-3 text-sm ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20',
@@ -18,7 +19,7 @@ export const HeaderActionButton = forwardRef<
       aria-label="Header Action"
     >
       {children}
-    </button>
+    </div>
   )
 })
 
