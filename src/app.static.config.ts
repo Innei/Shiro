185b5d5ca85029bf6f8bfb951e6f06d5eb10f780
Innei <i@innei.in>
2024-04-23 16:05:28 +0800

    fix: s3 config
    Signed-off-by: Innei <i@innei.in>

--- src/app.static.config.ts
+++ src/app.static.config.ts
@@ -18,3 +18,11 @@ export const appStaticConfig = {
 
 export const CDN_HOST = 'cdn.innei.ren'
 export const TENCENT_CDN_DOMAIN = CDN_HOST
+
+export const s3Config = {
+  accessKeyId: process.env.S3_ACCESS_KEY as string,
+  secretAccessKey: process.env.S3_SECRET_KEY as string,
+  bucket: 'uploads',
+  customDomain: 'https://object.innei.in',
+  endpoint: `https://de7ecb0eaa0a328071255d557a6adb66.r2.cloudflarestorage.com`,
+}
