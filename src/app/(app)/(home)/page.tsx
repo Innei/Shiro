b9608de4de31e4e5bc138a103478146d929bd8c6
Innei <i@innei.in>
2024-04-28 21:07:20 +0800

    fix: hero text pos
    Signed-off-by: Innei <i@innei.in>

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -185,7 +185,7 @@ const Hero = () => {
             'text-neutral-800/80 center dark:text-neutral-200/80',
           )}
         >
-          <small>
+          <small className="text-center">
             当第一颗卫星飞向大气层外，我们便以为自己终有一日会征服宇宙。
           </small>
           <span className="mt-8 animate-bounce">
