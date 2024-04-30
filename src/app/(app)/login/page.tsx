bbd68f5d860c8c76104e0acdc0471c0683ab398a
Innei <i@innei.in>
2024-04-30 18:23:12 +0800

    fix: add `login` page copywrite
    Signed-off-by: Innei <i@innei.in>

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -1,16 +1,23 @@
 'use client'
 
-import { useState } from 'react'
+import { use, useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { StyledButton } from '~/components/ui/button'
 import { Input } from '~/components/ui/input/Input'
+import { apiClient } from '~/lib/request'
 import { Routes } from '~/lib/route-builder'
 
+const canLoginPromise = apiClient.proxy.user('allow-login').get<{
+  password: boolean
+  passkey: boolean
+}>()
 export default function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const router = useRouter()
+
+  const { passkey: canPasskey, password: canPassword } = use(canLoginPromise)
 
   const handleLogin = async (e: any) => {
     e.preventDefault()
@@ -25,28 +32,36 @@ export default function LoginPage() {
     }
   }
   return (
-    <div className="flex min-h-[calc(100vh-7rem)] center">
-      <form className="flex flex-col space-y-5" onSubmit={handleLogin}>
-        <Input
-          autoFocus
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          type="text"
-          placeholder="Username"
-        />
-        <Input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
+    <div className="center flex min-h-[calc(100vh-7rem)] flex-col">
+      <div className="mb-6 text-lg font-medium">
+        你发现了神秘的登录入口，但这并没有什么用
+      </div>
+      {canPassword && (
+        <form className="flex flex-col space-y-5" onSubmit={handleLogin}>
+          <Input
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
 
-        <div className="flex center">
-          <StyledButton disabled={!username || !password} onClick={handleLogin}>
-            Login
-          </StyledButton>
-        </div>
-      </form>
+          <div className="center flex">
+            <StyledButton
+              disabled={!username || !password}
+              onClick={handleLogin}
+            >
+              Login
+            </StyledButton>
+          </div>
+        </form>
+      )}
     </div>
   )
 }
