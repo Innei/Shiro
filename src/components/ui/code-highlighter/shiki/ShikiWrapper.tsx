10750a4b0f9f8157d7ac9421756876e4a2edb286
Innei <i@innei.in>
2024-05-02 13:17:03 +0800

    fix: comment markdown style
    Signed-off-by: Innei <i@innei.in>

--- src/components/ui/code-highlighter/shiki/ShikiWrapper.tsx
+++ src/components/ui/code-highlighter/shiki/ShikiWrapper.tsx
@@ -98,7 +98,7 @@ export const ShikiHighLighterWrapper = forwardRef<
 
   return (
     <div
-      className={clsx(styles['code-card'], 'group')}
+      className={clsx(styles['code-card'], 'shiki-block group')}
       onCopy={stopPropagation}
     >
       {!!filename && (
