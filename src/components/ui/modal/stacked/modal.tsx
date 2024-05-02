17928ea78bfd807926d4befe7ed4d1dfaf6bf231
Innei <i@innei.in>
2024-05-02 16:50:56 +0800

    fix: modal title bar
    Signed-off-by: Innei <i@innei.in>

--- src/components/ui/modal/stacked/modal.tsx
+++ src/components/ui/modal/stacked/modal.tsx
@@ -232,7 +232,7 @@ export const ModalInternal: Component<{
                 )}
                 onClick={stopPropagation}
               >
-                <div className="relative flex flex-1 items-center">
+                <div className="relative flex items-center">
                   <Dialog.Title className="shrink-0 grow items-center px-4 py-1 text-lg font-medium">
                     {title}
                   </Dialog.Title>
