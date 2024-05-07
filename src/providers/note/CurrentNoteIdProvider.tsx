272d86c3e46f51e5c8356e4fa9d705a32d4dcaf7
Innei <i@innei.in>
2024-05-07 13:51:21 +0800

    refactor: use hydrate atoms
    Signed-off-by: Innei <i@innei.in>

--- src/providers/note/CurrentNoteIdProvider.tsx
+++ src/providers/note/CurrentNoteIdProvider.tsx
@@ -2,9 +2,9 @@
 
 import { memo, useEffect } from 'react'
 import { atom, useAtomValue, useSetAtom } from 'jotai'
+import { useHydrateAtoms } from 'jotai/utils'
 import type { FC, PropsWithChildren } from 'react'
 
-import { useBeforeMounted } from '~/hooks/common/use-before-mounted'
 import { jotaiStore } from '~/lib/store'
 
 const currentNoteNidAtom = atom<null | string>(null)
@@ -14,9 +14,8 @@ export const CurrentNoteNidProvider: FC<
   } & PropsWithChildren
 > = memo(({ nid, children }) => {
   const setNoteId = useSetAtom(currentNoteNidAtom)
-  useBeforeMounted(() => {
-    // setNoteId(noteId)
-    jotaiStore.set(currentNoteNidAtom, nid)
+  useHydrateAtoms([[currentNoteNidAtom, nid]], {
+    dangerouslyForceHydrate: true,
   })
 
   useEffect(() => {
