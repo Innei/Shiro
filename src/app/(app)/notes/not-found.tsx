cd88a89c8727c6f67d8fe32bb1bbdf6a2c31d797
Innei <i@innei.in>
2024-05-14 22:14:58 +0800

    feat: note not found with token
    Signed-off-by: Innei <i@innei.in>

---
+++ src/app/(app)/notes/not-found.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { usePathname, useRouter } from 'next/navigation'
+
+import { useIsLogged } from '~/atoms/hooks'
+import { StyledButton } from '~/components/ui/button'
+import { getToken } from '~/lib/cookie'
+
+import NotFound404 from '../not-found'
+
+export default () => {
+  const isLogged = useIsLogged()
+  const pathname = usePathname()
+  const router = useRouter()
+
+  return (
+    <div className="center flex flex-col">
+      <NotFound404>
+        {isLogged && (
+          <div>
+            <StyledButton
+              onClick={() => {
+                router.replace(`${pathname}?token=${getToken()}`)
+
+                setTimeout(() => {
+                  location.reload()
+                }, 1000)
+              }}
+            >
+              携带 Token 后查看
+            </StyledButton>
+          </div>
+        )}
+      </NotFound404>
+    </div>
+  )
+}
