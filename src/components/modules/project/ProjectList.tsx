b20af88d92cba61e10e4cf38122b92248f6f7b2e
Innei <i@innei.in>
2024-07-15 00:12:12 +0800

    fix: project style
    Signed-off-by: Innei <i@innei.in>

--- src/components/modules/project/ProjectList.tsx
+++ src/components/modules/project/ProjectList.tsx
@@ -14,13 +14,13 @@ export type Project = {
 export const ProjectList: FC<{ projects: Project[] }> = (props) => {
   return (
     <section key="list" className="text-center">
-      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid min-w-0 grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {props.projects.map((project) => {
           return (
             <Link
               href={routeBuilder(Routes.Project, { id: project.id })}
               key={project.id}
-              className="group grid grid-cols-[1fr_2fr] gap-4"
+              className="group grid shrink-0 grid-cols-[60px_2fr] gap-4"
             >
               <ProjectIcon
                 className="group-hover:-translate-y-2 group-hover:shadow-out-sm"
@@ -28,7 +28,7 @@ export const ProjectList: FC<{ projects: Project[] }> = (props) => {
                 name={project.name}
               />
               <span className="flex shrink-0 grow flex-col gap-2 text-left">
-                <h4 className="font-2xl m-0 p-0 font-medium">{project.name}</h4>
+                <h4 className="m-0 p-0 font-medium">{project.name}</h4>
                 <span className="line-clamp-5 text-sm md:line-clamp-4 lg:line-clamp-2">
                   {project.description}
                 </span>
