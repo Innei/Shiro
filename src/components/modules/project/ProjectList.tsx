b7791a02d939dc443f43dd2a20fabb9cae631e04
Innei <i@innei.in>
2024-07-15 00:09:35 +0800

    Revert "fix: projects layout & styles (#399) (#401)"
    This reverts commit 432f005a1c8afeca45563b602d8965de37d495c4.

--- src/components/modules/project/ProjectList.tsx
+++ src/components/modules/project/ProjectList.tsx
@@ -14,13 +14,13 @@ export type Project = {
 export const ProjectList: FC<{ projects: Project[] }> = (props) => {
   return (
     <section key="list" className="text-center">
-      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {props.projects.map((project) => {
           return (
             <Link
               href={routeBuilder(Routes.Project, { id: project.id })}
               key={project.id}
-              className="group grid grid-cols-[6em_auto] gap-4"
+              className="group grid grid-cols-[1fr_2fr] gap-4"
             >
               <ProjectIcon
                 className="group-hover:-translate-y-2 group-hover:shadow-out-sm"
@@ -28,8 +28,8 @@ export const ProjectList: FC<{ projects: Project[] }> = (props) => {
                 name={project.name}
               />
               <span className="flex shrink-0 grow flex-col gap-2 text-left">
-                <h4 className="m-0 p-0 text-2xl font-medium">{project.name}</h4>
-                <span className="line-clamp-5 opacity-60 md:line-clamp-4 lg:line-clamp-2">
+                <h4 className="font-2xl m-0 p-0 font-medium">{project.name}</h4>
+                <span className="line-clamp-5 text-sm md:line-clamp-4 lg:line-clamp-2">
                   {project.description}
                 </span>
               </span>
