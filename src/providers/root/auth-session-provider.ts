fdd11ea00c4b702ff6eebf5f797c7912d10a8e16
Innei <i@innei.in>
2024-09-02 22:38:41 +0800

    fix: is owner
    Signed-off-by: Innei <i@innei.in>

--- src/providers/root/auth-session-provider.ts
+++ src/providers/root/auth-session-provider.ts
@@ -1,7 +1,9 @@
 import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 
+import { isLoggedAtom } from '~/atoms'
 import { setSessionReader } from '~/atoms/hooks/reader'
+import { jotaiStore } from '~/lib/store'
 import type { SessionReader } from '~/models/session'
 
 declare module 'next-auth' {
@@ -13,6 +15,7 @@ export const AuthSessionProvider: Component = ({ children }) => {
 
   useEffect(() => {
     setSessionReader(session.data)
+    if (session.data?.isOwner) jotaiStore.set(isLoggedAtom, true)
   }, [session.data])
   return children
 }
