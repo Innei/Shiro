6e0d9500ee2247ab65fc27c96ceaf13ffa806073
Innei <i@innei.in>
2024-09-13 23:13:19 +0800

    fix(oauth): hide when no provider
    Signed-off-by: Innei <i@innei.in>

--- src/components/modules/comment/CommentBox/SignedOutContent.tsx
+++ src/components/modules/comment/CommentBox/SignedOutContent.tsx
@@ -2,18 +2,24 @@
 
 import { useSessionReader } from '~/atoms/hooks/reader'
 import { StyledButton } from '~/components/ui/button'
-import { AuthProvidersRender } from '~/queries/hooks/authjs'
+import { AuthProvidersRender, useAuthProviders } from '~/queries/hooks/authjs'
 
 import { CommentBoxMode, setCommentMode } from './hooks'
 
 export function CommentBoxSignedOutContent() {
   const isReaderLogin = !!useSessionReader()
+  const providers = useAuthProviders()
+  const hasProviders = providers && Object.keys(providers).length > 0
   if (isReaderLogin) return null
 
   return (
     <div className="center flex h-[150px] w-full flex-col rounded-lg bg-gray-100/80 dark:bg-zinc-900/80">
-      <p className="mb-4 text-sm">使用社交账号登录</p>
-      <AuthProvidersRender />
+      {hasProviders && (
+        <>
+          <p className="mb-4 text-sm">使用社交账号登录</p>
+          <AuthProvidersRender />
+        </>
+      )}
 
       <StyledButton
         className="mt-6"
