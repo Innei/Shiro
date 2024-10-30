58f5b5230bb89bcefc4ca2945f54ef9513017616
Innei <tukon479@gmail.com>
2024-10-30 15:21:03 +0800

    fix: comment modal overlay
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/modules/shared/CommentModal.tsx
+++ src/components/modules/shared/CommentModal.tsx
@@ -18,7 +18,7 @@ export const CommentModal: ModalContentComponent<CommentModalProps> = (
   const { dismiss } = useCurrentModal()
 
   return (
-    <div className="overflow-y-auto overflow-x-hidden md:w-[500px] lg:w-[600px] xl:w-[700px]">
+    <div className="overflow-hidden md:w-[500px] lg:w-[600px] xl:w-[700px]">
       <span>
         回复： <h1 className="mt-4 text-lg font-medium">{title}</h1>
       </span>
