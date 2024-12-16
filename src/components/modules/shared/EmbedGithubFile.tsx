8e8b893382b24020754a73fea7367a7ad920ac2c
Do1e <dpj.email@qq.com>
2024-12-16 23:46:39 +0800

    feat: support hash in github link

--- src/components/modules/shared/EmbedGithubFile.tsx
+++ src/components/modules/shared/EmbedGithubFile.tsx
@@ -1,5 +1,7 @@
+
 import { useQuery } from '@tanstack/react-query'
-import React, { memo } from 'react'
+import * as React from 'react'
+import { memo } from 'react'
 
 import { HighLighterPrismCdn } from '../../ui/code-highlighter'
 import { Loading } from '../../ui/loading'
@@ -61,11 +63,15 @@ export const EmbedGithubFile = memo(
     owner,
     path,
     repo,
+    startLineNumber,
+    endLineNumber,
     refType,
   }: {
     owner: string
     repo: string
     path: string
+    startLineNumber: number
+    endLineNumber?: number
     refType?: string
   }) => {
     const ext = path.slice(path.lastIndexOf('.'))
@@ -94,7 +100,7 @@ export const EmbedGithubFile = memo(
 
     if (isError) {
       return (
-        <pre className="flex h-[50vh] flex-wrap rounded-md border border-uk-orange-light center">
+        <pre className="center flex h-[50vh] flex-wrap rounded-md border border-uk-orange-light">
           <code>Loading GitHub File Preview Failed:</code>
           <br />
           <code>
@@ -106,11 +112,25 @@ export const EmbedGithubFile = memo(
 
     if (!data) return null
 
-    return (
-      <div className="h-[50vh] w-full overflow-auto">
-        <HighLighterPrismCdn content={data} lang={fileType} />
-      </div>
-    )
+    const splitData = data.split('\n')
+    if (!endLineNumber) {
+      endLineNumber = splitData.length
+    }
+    const newData = splitData.slice(startLineNumber, endLineNumber).join('\n')
+
+    if (endLineNumber - startLineNumber > 20) {
+      return (
+        <div className="h-[50vh] w-full overflow-auto">
+          <HighLighterPrismCdn content={newData} lang={fileType} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="w-full overflow-auto">
+          <HighLighterPrismCdn content={newData} lang={fileType} />
+        </div>
+      )
+    }
   },
 )
 EmbedGithubFile.displayName = 'EmbedGithubFile'
