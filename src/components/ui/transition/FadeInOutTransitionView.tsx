5d01a4df3db0730563fbf9f1e1f39b715db25732
Innei <tukon479@gmail.com>
2025-01-09 00:21:20 +0800

    fix: type error
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/ui/transition/FadeInOutTransitionView.tsx
+++ src/components/ui/transition/FadeInOutTransitionView.tsx
@@ -1,13 +1,8 @@
 'use client'
 
-import type { FC, PropsWithChildren } from 'react'
-
 import { createTransitionView } from './factor'
-import type { BaseTransitionProps } from './typings'
 
-export const FadeInOutTransitionView: FC<
-  PropsWithChildren<BaseTransitionProps>
-> = createTransitionView({
+export const FadeInOutTransitionView = createTransitionView({
   from: {
     opacity: 0.001,
   },
