7ebe624891f31885e0bf88d6e17c8df12c92cdbd
Suemor <suemor233@outlook.com>
2025-04-15 15:25:10 +0800

    fix: downgrade tailwind css to v3 (#576)

--- src/components/ui/excalidraw/Excalidraw.tsx
+++ src/components/ui/excalidraw/Excalidraw.tsx
@@ -1,10 +1,10 @@
 import { Excalidraw as Board, exportToBlob } from '@excalidraw/excalidraw'
-import type { ExcalidrawElement } from '@excalidraw/excalidraw/types/element/types'
+import type { ExcalidrawElement } from '@excalidraw/excalidraw/element/types'
 import type {
   AppState,
   BinaryFiles,
   ExcalidrawImperativeAPI,
-} from '@excalidraw/excalidraw/types/types'
+} from '@excalidraw/excalidraw/types'
 import { useQuery } from '@tanstack/react-query'
 import type { Delta } from 'jsondiffpatch'
 import { patch } from 'jsondiffpatch'
@@ -211,7 +211,7 @@ const ExcalidrawImpl = forwardRef<InternelExcalidrawRefObject, ExcalidrawProps>(
                   elements,
                   files: null,
                 })
-                blob.then((blob) => {
+                blob.then((blob: Blob) => {
                   win?.location.replace(URL.createObjectURL(blob))
                 })
               } else {
